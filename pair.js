a3195a81066870c4d933e700d95798b6adf3a9d9
datamate-lanka <datamate.lanka@gmail.com>
2024-12-07 13:23:46 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -21,10 +21,10 @@ function removeFile(filePath) {
 router.get('/', async (req, res) => {
     let num = req.query.number;
 
-    async function PrabathPair() {
+    async function DataMatePair() {
         const { state, saveCreds } = await useMultiFileAuthState(`./session`);
         try {
-            let DataMateSessionGenarator = makeWASocket({
+            let DataMateSessionGenerator = makeWASocket({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
@@ -34,40 +34,40 @@ router.get('/', async (req, res) => {
                 browser: Browsers.macOS("Safari"),
             });
 
-            if (!DataMateSessionGenarator.authState.creds.registered) {
+            if (!DataMateSessionGenerator.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-                const code = await DataMateSessionGenarator.requestPairingCode(num);
+                const code = await DataMateSessionGenerator.requestPairingCode(num);
                 if (!res.headersSent) {
                     res.send({ code });
                 }
             }
 
-            DataMateSessionGenarator.ev.on('creds.update', saveCreds);
-            DataMateSessionGenarator.ev.on("connection.update", async (s) => {
+            DataMateSessionGenerator.ev.on('creds.update', saveCreds);
+            DataMateSessionGenerator.ev.on("connection.update", async (s) => {
                 const { connection, lastDisconnect } = s;
                 if (connection === "open") {
                     try {
                         await delay(10000);
                         const authPath = './session/creds.json';
-                        const userJid = jidNormalizedUser(DataMateSessionGenarator.user.id);
+                        const userJid = jidNormalizedUser(DataMateSessionGenerator.user.id);
 
                         if (fs.existsSync(authPath)) {
                             // Send the creds.json file first
-                            const credsMessage = await DataMateSessionGenarator.sendMessage(userJid, {
+                            const credsMessage = await DataMateSessionGenerator.sendMessage(userJid, {
                                 document: { url: authPath },
                                 mimetype: "application/json",
                                 fileName: "creds.json",
                             });
 
                             // Send follow-up message
-                            await DataMateSessionGenarator.sendMessage(userJid, {
+                            await DataMateSessionGenerator.sendMessage(userJid, {
                                 text: "❌ Don't send this file to anyone. \n\n - Upload this file to `session_file_here` in your bot repository. \n\n > DataMate Inc",
                                 contextInfo: { quotedMessage: credsMessage.message }, // Reply to creds.json
                             });
                         }
                     } catch (e) {
-                        exec('pm2 restart prabath');
+                        exec('pm2 restart datamate');
                     }
 
                     await delay(100);
@@ -75,25 +75,25 @@ router.get('/', async (req, res) => {
                     process.exit(0);
                 } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
                     await delay(10000);
-                    PrabathPair();
+                    DataMatePair();
                 }
             });
         } catch (err) {
-            exec('pm2 restart prabath-md');
+            exec('pm2 restart datamate-md');
             console.log("Service restarted");
-            PrabathPair();
+            DataMatePair();
             removeFile('./session');
             if (!res.headersSent) {
                 res.send({ code: "Service Unavailable" });
             }
         }
     }
-    return await PrabathPair();
+    return await DataMatePair();
 });
 
 process.on('uncaughtException', function (err) {
     console.log('Caught exception: ' + err);
-    exec('pm2 restart prabath');
+    exec('pm2 restart datamate');
 });
 
 module.exports = router;
